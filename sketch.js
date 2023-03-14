24b02469c2d1ec149a0a9134b9eb1d510bcffb8e
NotxarbTech <notxarb64079@gmail.com>
2023-03-13 23:33:45 -0500

    Made the canvas take up the entire page.

--- sketch.js
+++ sketch.js
@@ -2,7 +2,7 @@ let a = 0;
 let b = 5;
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
@@ -11,8 +11,8 @@ function draw() {
   noFill();
   stroke(255);
   beginShape();
-  strokeWeight(sqrt((mouseX - width/2)*(mouseX - width/2) + (mouseY - height/2)*(mouseY - height/2))/15);
-  for (let theta = 0; theta < 55; theta += 0.1) {
+  strokeWeight(sqrt((mouseX - width/2)*(mouseX - width/2) + (mouseY - height/2)*(mouseY - height/2))/30);
+  for (let theta = 0; theta < PI*100; theta += 0.1) {
     let r = a+b*theta;
     let x = r * cos(theta);
     let y = r * sin(theta);
